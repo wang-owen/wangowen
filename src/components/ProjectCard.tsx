a07fe6b1dbdc521846c4cb55764d70ebe1bc306a
Owen Wang <owenwang0099@gmail.com>
2024-05-17 19:31:38 -0400

    projects page

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,30 @@
+const ProjectCard = ({
+    title,
+    link,
+    description,
+}: {
+    title: string;
+    link: string;
+    description: String[];
+}) => {
+    return (
+        <div className="p-5 h-96 font-serif">
+            <a href={link} target="_blank" className="text-xl mb-3 hover:underline">
+                {title}
+            </a>
+            <div>
+                <div className="text-base leading-6">
+                    <ul>
+                        {description.map((point) => (
+                            <li className="list-disc" key={point.toString()}>
+                                {point}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProjectCard;
