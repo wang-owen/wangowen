cef2280623614d9646953c2cea0ea845ee22ed18
Owen Wang <owenwang0099@gmail.com>
2024-06-23 14:10:01 -0400

    added link to carousel items

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -5,56 +5,65 @@ const Carousel = () => {
         <div className="carousel carousel-center max-w-full space-x-4 p-4">
             <div className="carousel-item">
                 <CarouselCard
-                    label="Brook"
                     img="https://github.com/wang-owen/Brook/blob/main/client/public/screenshots/homepage.png?raw=true"
+                    label="Brook"
+                    link="https://github.com/wang-owen/Brook"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Internsheet"
                     img="https://github.com/wang-owen/Internsheet/assets/69203168/98f1a7a1-c289-4d77-a33b-2f5b58d9f93c"
+                    link="https://github.com/wang-owen/Internsheet"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="JBKR Mobile"
                     img="https://github.com/wang-owen/JBKR-Mobile/assets/69203168/9bc91515-cfc4-459c-a814-54f108e8fa7a"
+                    link="https://github.com/wang-owen/JBKR-Mobile"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Local Superchat"
                     img="https://github.com/wang-owen/local-superchat/assets/69203168/dd957db0-08eb-4535-aab2-ce86a2eefec3"
+                    link="https://github.com/wang-owen/local-superchat"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Checkers"
                     img="https://github.com/wang-owen/Checkers/assets/69203168/7a812183-bb4a-47c9-b1ec-18eb686d379c"
+                    link="https://github.com/wang-owen/Checkers"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Sorting Visualizer"
                     img="https://github.com/wang-owen/SortingVisualizer/assets/69203168/ba920749-eba3-4999-9d65-b7eed7b411db"
+                    link="https://github.com/wang-owen/SortingVisualizer"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Snakedy Dakedy"
                     img="https://github.com/wang-owen/SnakedyDakedy/assets/69203168/7ea0dd52-66cb-4629-8426-99987372a238"
+                    link="https://github.com/wang-owen/SnakedyDakedy"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="The Wildfire Network"
                     img="https://github.com/wang-owen/TheWildfireNetwork/assets/69203168/22a007d6-d469-4b7c-bbc2-75e4c484e0f6"
+                    link="https://github.com/wang-owen/TheWildfireNetwork"
                 />
             </div>
             <div className="carousel-item">
                 <CarouselCard
                     label="Stuckdoku"
                     img="https://github.com/wang-owen/Stuckdoku/assets/69203168/6cd67d09-1064-40c2-9cb3-9931708de7a5"
+                    link="https://github.com/wang-owen/Stuckdoku"
                 />
             </div>
         </div>
--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,10 +1,20 @@
-const CarouselCard = ({ img, label }: { img: string; label: string }) => {
+const CarouselCard = ({
+    img,
+    label,
+    link,
+}: {
+    img: string;
+    label: string;
+    link: string;
+}) => {
     return (
         <div className="relative text-center flex flex-col gap-4">
-            <img
-                className="object-scale-down h-48 hover:scale-110 hover:z-10 duration-300"
-                src={img}
-            />
+            <a href={link} target="_blank">
+                <img
+                    className="object-scale-down h-48 hover:scale-110 hover:z-10 hover:cursor-ne-resize duration-300"
+                    src={img}
+                />
+            </a>
             <label>{label}</label>
         </div>
     );
