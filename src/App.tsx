7c28e065a1c2932be609589c0bf1fac78d1c950e
Owen Wang <owenwang0099@gmail.com>
2024-06-23 10:07:53 -0400

    deleted resume page

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
-import ResumePage from "./pages/ResumePage";
 
 function App() {
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<HomePage />} />
-                <Route path="/resume" element={<ResumePage />} />
                 <Route path="/projects" element={<ProjectsPage />} />
             </Route>
         )
