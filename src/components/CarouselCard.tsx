f06f3a4b6c73162965aec2bb643421a3ec8ea856
Owen Wang <owenwang0099@gmail.com>
2024-06-23 14:52:28 -0400

    minor mobile change

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -11,7 +11,7 @@ const CarouselCard = ({
         <div className="relative text-center flex flex-col gap-4">
             <a href={link} target="_blank">
                 <img
-                    className="object-scale-down h-48 hover:scale-110 hover:z-10 hover:cursor-ne-resize duration-300"
+                    className="object-scale-down h-48 lg:hover:scale-110 lg:hover:z-10 hover:cursor-ne-resize duration-300"
                     src={img}
                 />
             </a>
