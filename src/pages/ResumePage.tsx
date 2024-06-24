ea900b50437eb80e05b18b29d7d7065ed400a13d
Owen Wang <owenwang0099@gmail.com>
2024-06-23 20:40:36 -0400

    minor change

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -2,19 +2,21 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 
 const ResumePage = () => {
     return (
-        <section className="animate-fadeIn h-full">
+        <section className="animate-fadeIn">
             <div className="flex justify-center h-full">
-                <div className="h-full w-full lg:w-2/3 flex justify-end">
-                    <div className="absolute m-6 hover:opacity-50 duration-300">
-                        <a href="Owen Wang Resume.pdf" target="_blank">
-                            <FaExternalLinkAlt size={25} color="black" />
-                        </a>
+                <div className="flex justify-center h-full w-full lg:w-2/3">
+                    <div className="h-full w-full flex justify-end">
+                        <iframe
+                            src="Owen Wang Resume.pdf"
+                            width="100%"
+                            height="100%"
+                        />
                     </div>
-                    <iframe
-                        src="Owen Wang Resume.pdf"
-                        width="100%"
-                        height="100%"
-                    />
+                </div>
+                <div className="self-end m-4 hover:opacity-50 duration-300 absolute lg:relative right-0 lg:right-auto">
+                    <a href="Owen Wang Resume.pdf" target="_blank">
+                        <FaExternalLinkAlt size={25} />
+                    </a>
                 </div>
             </div>
         </section>
