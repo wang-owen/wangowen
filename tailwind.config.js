6623d6eefd9885d1ae63f3baba383f29e576f5d0
Owen Wang <owenwang0099@gmail.com>
2024-12-07 14:32:04 -0500

    daisyui require bugfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui";
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             fontFamily: {
-                openSans: ["Open Sans"]
+                openSans: ["Open Sans"],
             },
             keyframes: {
                 fadeIn: {
@@ -49,5 +52,5 @@ export default {
             },
         },
     },
-    plugins: [require("daisyui")],
+    plugins: [daisyui],
 };
